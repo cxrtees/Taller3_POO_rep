ab77024c09a68a617fd89e47e20591ea23d869ac
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-16 19:34:24 -0300

    cambios a atributos

--- Taller3_POO_uwu/src/Taller/Proyecto.java
+++ Taller3_POO_uwu/src/Taller/Proyecto.java
@@ -1,8 +1,10 @@
 package Taller;
 
+import java.util.ArrayList;
+
 public class Proyecto {
 	private String ID;
 	private String Nombre;
 	private Usuario Responsable;
-
+	private ArrayList<Tarea> tareas;
 }

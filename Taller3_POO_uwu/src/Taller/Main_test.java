89bd13d52b302aa907edc0d2bba76689ffa278f3
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-16 03:55:53 -0300

    test
    prueba

--- Taller3_POO_uwu/src/Taller/Main_test.java
+++ Taller3_POO_uwu/src/Taller/Main_test.java
@@ -5,6 +5,8 @@ public class Main_test {
 	public static void main(String[] args) {
 		System.out.println("wasd");
 		System.out.println("ewe");
+		System.out.println("owowowoow");
+		//hola hola
 	}
 
 }

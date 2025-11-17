ed3c8bd565814489c31e37b177c94d973dd1549c
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-17 17:36:59 -0300

    Create PrioridadPorFecha.java

---
+++ Taller3_POO_uwu/src/Taller/PrioridadPorFecha.java
@@ -0,0 +1,13 @@
+package Taller;
+
+import java.util.ArrayList;
+
+public class PrioridadPorFecha implements EstrategiaPrioridad {
+
+	@Override
+	public void ordenar(ArrayList<Tarea> tareas) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

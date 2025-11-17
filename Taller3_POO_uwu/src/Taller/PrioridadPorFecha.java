c2679c2e202410388cbea36be302012d108a9355
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-17 17:59:27 -0300

    creacion de prioridad por fecha

--- Taller3_POO_uwu/src/Taller/PrioridadPorFecha.java
+++ Taller3_POO_uwu/src/Taller/PrioridadPorFecha.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 
 public class PrioridadPorFecha implements EstrategiaPrioridad {
 
-	@Override
+	
 	public void ordenar(ArrayList<Tarea> tareas) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < tareas.size() - 1; i++) {
+            for (int j = 0; j < tareas.size() - 1 - i; j++) {
+
+                Tarea t1 = tareas.get(j);
+                Tarea t2 = tareas.get(j + 1);
+
+                // si t1 tiene fecha MAYOR que t2, las cambio
+                if (t1.getFecha().compareTo(t2.getFecha()) > 0) {
+                    tareas.set(j, t2);
+                    tareas.set(j + 1, t1);
+                }
+            }
+        }
 
 	}
 

88e642e35b2673b123b534925ec20a32672beefb
cxrtees <benjamincortes2889@gmail.com>
2025-11-17 22:24:45 -0300

    Creacion CambiarEstadoVisitor

---
+++ Taller3_POO_uwu/src/Taller/CambiarEstadoVisitor.java
@@ -0,0 +1,20 @@
+package Taller;
+
+public class CambiarEstadoVisitor implements TareaVisitor {
+
+	@Override
+	public void visitBug(Bug bug) {
+		bug.setEstado("En progeso"); //bugs pasan a "En progreso"
+	}
+
+	@Override
+	public void visitFeature(Feature feature) {
+		feature.setEstado("Pendiente revisión"); //features quedan en "Pendiente revisión"
+	}
+
+	@Override
+	public void visitDocumentacion(Documentacion doc) {
+		doc.setEstado("Completada"); // documentación se marca como "Completada"
+	}
+
+}

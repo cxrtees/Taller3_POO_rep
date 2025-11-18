de5c2e623a8cfd346d7a566643385c12047aa83b
cxrtees <benjamincortes2889@gmail.com>
2025-11-17 22:11:28 -0300

    Accept en subclases
    se implemento los constructores y el metodo aceppt en subclases de Tarea

--- Taller3_POO_uwu/src/Taller/Documentacion.java
+++ Taller3_POO_uwu/src/Taller/Documentacion.java
@@ -2,4 +2,14 @@ package Taller;
 
 public class Documentacion extends Tarea {
 
+	public Documentacion(Taller.Proyecto proyecto, String iD, Tarea tipo, String descripcion, String estado,
+			Usuario responsable, String complejidad, String fecha) {
+		super(proyecto, iD, tipo, descripcion, estado, responsable, complejidad, fecha);
+	}
+
+	@Override
+	public void accept(TareaVisitor v) {
+		v.visitDocumentacion(this);
+	}
+
 }
--- Taller3_POO_uwu/src/Taller/Tarea.java
+++ Taller3_POO_uwu/src/Taller/Tarea.java
@@ -9,6 +9,58 @@ public abstract class Tarea {
 	private Usuario Responsable;
 	private String Complejidad;
 	private String fecha;
-	
 
+	public Tarea(Taller.Proyecto proyecto, String iD, Tarea tipo, String descripcion, String estado,
+			Usuario responsable, String complejidad, String fecha) {
+		Proyecto = proyecto;
+		ID = iD;
+		this.tipo = tipo;
+		Descripcion = descripcion;
+		Estado = estado;
+		Responsable = responsable;
+		Complejidad = complejidad;
+		this.fecha = fecha;
+	}
+	
+	public abstract void accept(TareaVisitor v);
+	
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+
+	public Proyecto getProyecto() {
+		return Proyecto;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public Tarea getTipo() {
+		return tipo;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public Usuario getResponsable() {
+		return Responsable;
+	}
+
+	public String getComplejidad() {
+		return Complejidad;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+	
+	
+	
+	
 }

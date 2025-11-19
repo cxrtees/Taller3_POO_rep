7faa31f12bf1d3838ee06e592709e221e1973e51
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-18 23:47:45 -0300

    Clase usuario

--- Taller3_POO_uwu/src/Taller/Usuario.java
+++ Taller3_POO_uwu/src/Taller/Usuario.java
@@ -5,6 +5,30 @@ public abstract class Usuario {
 	private String Contraseña;
 	private Usuario Rol;
 	
+	public Usuario(String username, String contraseña, Usuario rol) {
+		super();
+		Username = username;
+		Contraseña = contraseña;
+		Rol = rol;
+	}
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public String getContraseña() {
+		return Contraseña;
+	}
+
+	public Usuario getRol() {
+		return Rol;
+	}
+	
+	public boolean validarPassword(String pass) {// para validar contraseña
+	        return this.Contraseña.equals(pass);
+	    }
+
+public abstract void mostrarMenu(Sistema sistema); // clase abstracta para que cada uno implemente su menu
 	
 
 }

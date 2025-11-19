1453cefb504aedd5afdaef102d8908e9bb699692
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-19 00:53:20 -0300

    Update Administrador.java

--- Taller3_POO_uwu/src/Taller/Administrador.java
+++ Taller3_POO_uwu/src/Taller/Administrador.java
@@ -2,4 +2,21 @@ package Taller;
 
 public class Administrador extends Usuario {
 
+	public Administrador(String username, String contraseña) {
+		super(username, contraseña,"Admin");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mostrarMenu(Sistema sistema) {
+		sistema.MenuAdministradror(this);
+	
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [getUsername()=" + getUsername();
+	}
+	
+
 }

fe3a57980fb9a228cc38861ce5014ed82c3d76e0
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-20 03:45:48 -0300

    Update sistema.java
    modificando la interfaz sistema

--- Taller3_POO_uwu/src/Taller/sistema.java
+++ Taller3_POO_uwu/src/Taller/sistema.java
@@ -1,8 +1,23 @@
 package Taller;
 
-public interface sistema {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
+public interface sistema {
+	//menus
 	void MenuAdministrador(Administrador administrador);
 	void MenuColaborador(Colaborador colaborador);
+	
+	 // Búsquedas
+    Proyecto buscarProyecto(String id);
+    Usuario buscarUsuario(String username);
+    ArrayList<Tarea> tareasDe(Colaborador c);
+    
+    // Carga inicial
+    void cargarUsuarios() throws FileNotFoundException;
+    void cargarProyectos() throws FileNotFoundException;
+    void cargarTareas() throws FileNotFoundException;
+
+
 
 }

7375269a9c12721dbf0602a1b36429e1e6586db6
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-20 03:46:18 -0300

    Update sistema.java

--- Taller3_POO_uwu/src/Taller/sistema.java
+++ Taller3_POO_uwu/src/Taller/sistema.java
@@ -2,6 +2,7 @@ package Taller;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public interface sistema {
 	//menus
@@ -18,6 +19,24 @@ public interface sistema {
     void cargarProyectos() throws FileNotFoundException;
     void cargarTareas() throws FileNotFoundException;
 
+    // Funciones del Admin
+    void listarProyectosYTareasOrdenadas();
+    void agregarProyecto(Scanner s);
+    void eliminarProyecto(Scanner s);
+    void agregarTarea(Scanner s);
+    void eliminarTarea(Scanner s);
+    
+    // Utilidad
+    int leerEntero(Scanner s);
+    void elegirEstrategia(Scanner s);
+    void generarReporte();
+    
+    // Funciones básicas
+    void listarProyectosBasico();
 
+    // Funciones del Colaborador
+    void verMisTareasOrdenadas(Colaborador c);
+    void cambiarEstadoManual(Colaborador c, Scanner s);
+}
 
 }

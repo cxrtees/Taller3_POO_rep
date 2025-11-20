e611777caf14968e9e7463a407715fd6886c9c10
Catalina Rojas <cataa.rojas100@gmail.com>
2025-11-20 16:45:39 -0300

    Update Administrador.java

--- Taller3_POO_uwu/src/Taller/Administrador.java
+++ Taller3_POO_uwu/src/Taller/Administrador.java
@@ -7,15 +7,15 @@ public class Administrador extends Usuario {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void mostrarMenu(Sistema sistema) {
-		sistema.MenuAdministradror(this);
-	
-	}
 
 	@Override
 	public String toString() {
 		return "Administrador [getUsername()=" + getUsername();
+	}
+
+	@Override
+	public void mostrarMenu(sistema sistema) {
+		sistema.MenuAdministrador(this);
 	}
 	
 
